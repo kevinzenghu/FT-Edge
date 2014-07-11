f41c8e721a44d0d8bc94a8baa849c2d3df15e5e6
Matthew Andrews <matt@mattandre.ws>
2014-07-11 10:37:25 +0100

    Small refactor to centralise active-ness

--- FT-Dish.js
+++ FT-Dish.js
@@ -1,17 +1,16 @@
 if (Meteor.isClient) {
     Template.sections.events({
-        'touchend section': function(e) {
+        'click section': function(e) {
             console.log('Left pane');
-            $("div#left-pane, div#right-pane").removeClass('active');
-        },
-        'mousedown section, touchstart section': function(e) {
-            $(e.currentTarget).addClass('active');
+            $("body").addClass('active-left').removeClass('active-right');
         },
         'click section': function(e) {
             console.log('Right pane');
-            $(e.currentTarget).removeClass('active');
-            $("div#left-pane, div#right-pane").addClass('active');
+            $("body").removeClass('active-left').addClass('active-right');
         },
+        'click .back': function(e) {
+            $("body").removeClass('active-right').addClass('active-left');
+        }
 
     });
 
