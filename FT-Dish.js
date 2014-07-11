34860320876faf84091d94303a1d69971de4228b
Kevin Hu <kzh@Kevins-MacBook-Air-2.local>
2014-07-11 10:42:46 +0100

    Merged and adding specific pages

--- FT-Dish.js
+++ FT-Dish.js
@@ -3,7 +3,19 @@ if (Meteor.isClient) {
         'click section': function(e) {
             console.log('Left pane');
             $("body").addClass('active-left').removeClass('active-right');
-        }
+        },
+        'click section#starters': function(e) {
+            Session.set('currentPane', 'starters');
+        },
+        'click section#substance': function(e) {
+            Session.set('currentPane', 'substance');
+        },
+        'click section#a-drink-with': function(e) {
+            Session.set('currentPane', 'drink');
+        },
+        'click section#the-scoop': function(e) {
+            Session.set('currentPane', 'scoop');
+        },
     });
     Template.left_pane.events({
         'click section': function(e) {
@@ -16,6 +28,22 @@ if (Meteor.isClient) {
             $("body").removeClass('active-right').addClass('active-left');
         }
     });
+
+    Template.right_pane.helpers({
+        starters: function(e) {
+            return Session.equals('currentPane', 'starters')
+        },
+        substance: function(e) {
+            return Session.equals('currentPane', 'substance')
+        },
+        drink: function(e) {
+            return Session.equals('currentPane', 'drink')
+        },
+        scoop: function(e) {
+            return Session.equals('currentPane', 'scoop')
+        },
+    })
+
     Meteor.startup(function() {
         FastClick.attach(document.body);
     });
