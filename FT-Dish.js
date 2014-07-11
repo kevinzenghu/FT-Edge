0c996ad1e673caf30b97f565259cc005d92c2869
Matthew Andrews <matt@mattandre.ws>
2014-07-11 10:40:32 +0100

    Back button

--- FT-Dish.js
+++ FT-Dish.js
@@ -3,17 +3,19 @@ if (Meteor.isClient) {
         'click section': function(e) {
             console.log('Left pane');
             $("body").addClass('active-left').removeClass('active-right');
-        },
+        }
+    });
+    Template.left_pane.events({
         'click section': function(e) {
             console.log('Right pane');
             $("body").removeClass('active-left').addClass('active-right');
-        },
-        'click .back': function(e) {
+        }
+    });
+    Template.right_pane.events({
+        'click header': function(e) {
             $("body").removeClass('active-right').addClass('active-left');
         }
-
     });
-
     Meteor.startup(function() {
         FastClick.attach(document.body);
     });
